aee9ea34849c9d5ecedf0a4580707ea0a0330f7d
SevJSix <iansevier04@gmail.com>
2022-06-09 16:38:24 -0700

    a

--- src/main/java/me/sevj6/illegalfucker/util/ItemUtil.java
+++ src/main/java/me/sevj6/illegalfucker/util/ItemUtil.java
@@ -1,5 +1,6 @@
 package me.sevj6.illegalfucker.util;
 
+import net.minecraft.server.v1_12_R1.ItemStack;
 import org.bukkit.Material;
 
 import java.util.Arrays;
@@ -10,4 +11,17 @@ public class ItemUtil {
     public static final List<Material> unobtainable = Arrays.asList(Material.BEDROCK, Material.LONG_GRASS, Material.MOB_SPAWNER, Material.SOIL,
             Material.COMMAND, Material.COMMAND_REPEATING, Material.COMMAND_CHAIN, Material.COMMAND_MINECART, Material.BARRIER, Material.GRASS_PATH,
             Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.MONSTER_EGGS, Material.MONSTER_EGG, Material.KNOWLEDGE_BOOK);
+
+    public static boolean isOverstacked(ItemStack item) {
+        return item.getCount() > item.getMaxStackSize();
+    }
+
+    public static boolean isOverEnchanted(ItemStack item) {
+        return item.hasEnchantments() && item.asBukkitCopy().getEnchantments().entrySet().stream().anyMatch(entry -> entry.getValue() > entry.getKey().getMaxLevel());
+    }
+
+    public static boolean isIllegalDurability(ItemStack item) {
+        org.bukkit.inventory.ItemStack itemStack = item.asBukkitCopy();
+        return ((itemStack.getType().getMaxDurability() > 50) && (itemStack.getDurability() < 0 || itemStack.getDurability() > itemStack.getType().getMaxDurability()));
+    }
 }
