c76c49208bedac09057e45041cf7a3f879e36d7b
SevJSix <iansevier04@gmail.com>
2022-06-09 16:25:39 -0700

    itemutil

---
+++ src/main/java/me/sevj6/illegalfucker/util/ItemUtil.java
@@ -0,0 +1,13 @@
+package me.sevj6.illegalfucker.util;
+
+import org.bukkit.Material;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemUtil {
+
+    public static final List<Material> unobtainable = Arrays.asList(Material.BEDROCK, Material.LONG_GRASS, Material.MOB_SPAWNER, Material.SOIL,
+            Material.COMMAND, Material.COMMAND_REPEATING, Material.COMMAND_CHAIN, Material.COMMAND_MINECART, Material.BARRIER, Material.GRASS_PATH,
+            Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.MONSTER_EGGS, Material.MONSTER_EGG, Material.KNOWLEDGE_BOOK);
+}
