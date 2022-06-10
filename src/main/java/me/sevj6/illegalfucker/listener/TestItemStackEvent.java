347aa979135d22ce29f5701c9ee93773f9e64d11
SevJSix <iansevier04@gmail.com>
2022-06-09 17:07:26 -0700

    cool commit

--- src/main/java/me/sevj6/illegalfucker/listener/TestItemStackEvent.java
+++ src/main/java/me/sevj6/illegalfucker/listener/TestItemStackEvent.java
@@ -1,6 +1,8 @@
 package me.sevj6.illegalfucker.listener;
 
+import me.sevj6.illegalfucker.util.ItemUtil;
 import me.txmc.paperapiextentions.events.ItemStackCreateEvent;
+import me.txmc.paperapiextentions.mixin.mixins.MixinItemStack;
 import net.minecraft.server.v1_12_R1.ItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,5 +12,6 @@ public class TestItemStackEvent implements Listener {
     @EventHandler
     public void onCreate(ItemStackCreateEvent event) {
         ItemStack item = event.getItemStack();
+        if (ItemUtil.isIllegal(item)) event.setCancelled(true);
     }
 }
--- src/main/java/me/sevj6/illegalfucker/util/ItemUtil.java
+++ src/main/java/me/sevj6/illegalfucker/util/ItemUtil.java
@@ -1,6 +1,8 @@
 package me.sevj6.illegalfucker.util;
 
+import net.minecraft.server.v1_12_R1.Item;
 import net.minecraft.server.v1_12_R1.ItemStack;
+import net.minecraft.server.v1_12_R1.Items;
 import org.bukkit.Material;
 
 import java.util.Arrays;
@@ -8,20 +10,30 @@ import java.util.List;
 
 public class ItemUtil {
 
-    public static final List<Material> unobtainable = Arrays.asList(Material.BEDROCK, Material.LONG_GRASS, Material.MOB_SPAWNER, Material.SOIL,
-            Material.COMMAND, Material.COMMAND_REPEATING, Material.COMMAND_CHAIN, Material.COMMAND_MINECART, Material.BARRIER, Material.GRASS_PATH,
-            Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.MONSTER_EGGS, Material.MONSTER_EGG, Material.KNOWLEDGE_BOOK);
+    public static final List<Item> illegals = Arrays.asList(
+            Item.getById(7), //Bedrock
+            Item.getById(166), //Barrier
+            Item.getById(120), // End portal frames
+            Item.getById(52), //Monster spawner
+            Item.getById(255), // Structure block
+            Item.getById(217), //Structure void
+            Item.getById(383), //Spawn egg
+            Item.getById(211), //Chain Command Block
+            Item.getById(210), //Repeating Command Block
+            Item.getById(137), //Command Block
+            Item.getById(422) //Command Block Minecart
+    );
+
+    public static boolean isIllegal(ItemStack item) {
+        if (item == null) return false;
+        return isOverstacked(item) || isUnobtainableItem(item);
+    }
 
     public static boolean isOverstacked(ItemStack item) {
         return item.getCount() > item.getMaxStackSize();
     }
 
-    public static boolean isOverEnchanted(ItemStack item) {
-        return item.hasEnchantments() && item.asBukkitCopy().getEnchantments().entrySet().stream().anyMatch(entry -> entry.getValue() > entry.getKey().getMaxLevel());
-    }
-
-    public static boolean isIllegalDurability(ItemStack item) {
-        org.bukkit.inventory.ItemStack itemStack = item.asBukkitCopy();
-        return ((itemStack.getType().getMaxDurability() > 50) && (itemStack.getDurability() < 0 || itemStack.getDurability() > itemStack.getType().getMaxDurability()));
+    public static boolean isUnobtainableItem(ItemStack item) {
+        return illegals.contains(item.getItem());
     }
 }
